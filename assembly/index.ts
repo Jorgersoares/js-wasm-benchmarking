835ba7c1f38662f3db8c977c520e7d8116600b69
Jorge Ricardo Soares <jorgersoares@outlook.com>
2020-12-13 18:22:40 -0300

    corrigindo estrutura

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,34 +1,4 @@
 // The entry file of your WebAssembly module.
-
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
-export function crivo(n: number): number {
-  const vetor: bool[] = [];
-
-  for (let i = 0; i <= n; i++) vetor[i] = true;
-
-  vetor[0] = vetor[1] = false;
-
-  let limit = Math.sqrt(n);
-
-  for (var i = 2; i <= limit; i++) {
-    if (vetor[i]) {
-      for (var j = i * i; j <= n; j += i) vetor[j] = false;
-    }
-  }
-  
-
-  let num: number = 0;
-
-  for (let i = 0; i < vetor.length; i++) {
-    if(unchecked(vetor[i])) num++
-  }
-
-  return num
-}
-
 export function fib(n: i32): i32{
   if (n <= 1) {
     return 0;
--- tests/crivo_test.js
+++
@@ -1,8 +0,0 @@
-const myModule = require("..");
-//const js = require("../javascript/fatorial")
-
-// const MSG_TESTE_WASM_FATO = 'teste wasm fatorial'
-// const MSG_TESTE_JS_FATO = 'teste js fatorial'
-
-console.log(myModule.ackermann(4,2))
-
